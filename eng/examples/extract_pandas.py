0f2d64d92d8a2aaaf457ff5ea7fe398ad7d9ebc7
nicki6 <nicolai.tesela@gmail.com>
2015-06-09 10:45:14 -0500

    added examples to class

--- eng/examples/extract_pandas.py
+++ eng/examples/extract_pandas.py
@@ -10,12 +10,12 @@ d = pd.read_csv('jan15.csv')
 #filterint out UNK values for blockranges, Double check with GIS
 d = d[d.BlockRange!='UNK']
 d.BlockRange = d.BlockRange.map(
-                                lambda x: int(
-                                                    np.mean(
-                                                            [int(i) for i in x.split('-')]
-                                                            )
-                                                        )
-                                                    )
+    lambda x: int(
+        np.mean(
+            [int(i) for i in x.split('-')]
+        )
+    )
+)
 
 d['Address'] = pd.DataFrame([' '.join([str(i[0]),i[1],i[2]]) for i in zip(d.BlockRange, d.StreetName, d.Type)], index=d.index)
 del d['BlockRange']
